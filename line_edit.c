02ac5609e70a935a960fa941682575e24d1edb29
Max Gautier <mgautier@student.42.fr>
2017-09-11 10:19:22 +0200

    First line edit draft

---
+++ line_edit.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   line_edit.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/11 09:28:05 by mgautier          #+#    #+#             */
+/*   Updated: 2017/09/11 10:19:10 by mgautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include "libft.h"
+
+char	*read_input(void)
+{
+	char	*total_input;
+	char	*current_input;
+	t_lst	*tmp_inputs;
+
+	while (!input_end(current_input))
+	{
+
+	}
+}
