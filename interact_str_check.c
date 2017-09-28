09a2ce370e0bd25efc7e9fb27e616969cd527c77
Max Gautier <mgautier@student.42.fr>
2017-09-28 11:47:35 +0200

    Updates int_str method to check quotes.
    Also add some minor comments (return codes).

--- interact_str_check.c
+++ interact_str_check.c
@@ -6,26 +6,32 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 16:38:06 by mgautier          #+#    #+#             */
-/*   Updated: 2017/09/27 19:06:15 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/09/28 11:14:59 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "interactive_string_defs.h"
 #include "libft.h"
 
-int	int_str_state_of_quote(t_interact_str const *str)
+int	int_str_state_of_quote(t_interact_str const *str,
+		char const *quote_chars)
 {
 	t_str_link	*current;
-	static char	quote_characters[] = {'"', '\'', '\0'};
-	t_bool		quote_states[ARRAY_LENGTH(quote_characters) - 1];
+	t_bool		*quote_states;
+	int			result;
+	size_t		size;
 
+	size = ft_strlen(quote_chars);
+	quote_states = create_bool_array(size, FALSE);
+	if (quote_states == NULL)
+		return (-1);
 	current = str->begin;
-	init_bool_array(quote_states, ARRAY_LENGTH(quote_states), FALSE);
 	while (current != str->end)
 	{
-		update_states(quote_states, current->letter, quote_characters);
+		update_states(quote_states, current->letter, quote_chars);
 		current = current->after;
 	}
-	return (return_result(quote_states, ARRAY_LENGTH(quote_characters) - 1));
+	result = return_result(quote_states, size);
+	destroy_bool_array(&quote_states, size);
+	return (result);
 }
-
